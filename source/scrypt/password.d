334fc6764aa6f45850510390c1f4c172081dd479
Isak Andersson <BitPuffin@lavabit.com>
2013-02-25 16:23:04 +0100

    Add basic password module and fill with boilerplate

---
+++ source/scrypt/password.d
@@ -0,0 +1,22 @@
+module scrypt.password;
+
+/*
+ * Copyright (C) 2013 Isak Andersson (BitPuffin@lavabit.com)
+ * 
+ * Distributed under the terms of the Zlib/libpng license
+ * See LICENSE.txt in project root for more info
+ */
+
+import scrypt.scryptenc;
+
+// TODO: Write docs
+string generateRandomSalt() {
+    // TODO: You'll know what to do
+    return "";
+}
+
+// TODO: Write docs
+string getEncodedPassword(string password, string salt) {
+    // TODO: You'll know what to do
+    return "";
+}
