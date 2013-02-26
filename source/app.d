e51abd4d308f0e03b4ae26f2059d97bc6ee7e1a1
Isak Andersson <BitPuffin@lavabit.com>
2013-02-26 14:43:45 +0100

    I am an idiot

--- source/app.d
+++ source/app.d
@@ -3,5 +3,5 @@ import scrypt.password;
 
 void main() {
     auto pw = genScryptPassword("my password", "penis");
-    writeln(checkScryptPassword("my password", pw) ? "it matched!" : "wtf");
+    writeln(checkScryptPassword(pw, "my password") ? "it matched!" : "wtf");
 }
