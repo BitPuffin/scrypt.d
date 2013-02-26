2570250cb85e0400dc05e4431a017d6fb3824edb
Isak Andersson <BitPuffin@lavabit.com>
2013-02-26 23:59:28 +0100

    It is now feature complete, ready for people to test!

--- source/app.d
+++ source/app.d
@@ -1,6 +1,14 @@
 import std.stdio;
 import scrypt.password;
 
+/// Just try stuff out
 void main() {
-    writeln("example: ", genScryptPassword("password", generateRandomSalt(), SCRYPT_OUTPUTLEN_DEFAULT, SCRYPT_N_DEFAULT, SCRYPT_R_DEFAULT, SCRYPT_P_DEFAULT, SCRYPT_SALT_SEPERATOR_DEFAULT)); 
+    writeln("example: ", genScryptPasswordHash("password", genRandomSalt(), SCRYPT_OUTPUTLEN_DEFAULT, SCRYPT_N_DEFAULT, SCRYPT_R_DEFAULT, SCRYPT_P_DEFAULT)); 
+    writeln(genScryptPasswordHash("pw", genRandomSalt(), 90, SCRYPT_N_DEFAULT, 8, 1) != genScryptPasswordHash("pw", genRandomSalt(), 90, SCRYPT_N_DEFAULT, 8, 1));
+
+    foreach(i; 1 .. 10)
+        writeln(genScryptPasswordHash("pw", genRandomSalt(), 90, SCRYPT_N_DEFAULT, 8, 1));
+
+    string pw = genScryptPasswordHash("very password nice really, random kind the you use should really", genRandomSalt(), 90, SCRYPT_N_DEFAULT, 8, 1);
+    writeln(checkScryptPasswordHash(pw, "very password nice really, random kind the you use should really", 90, SCRYPT_N_DEFAULT, 8, 1));
 }
--- source/scrypt/password.d
+++ source/scrypt/password.d
@@ -10,44 +10,42 @@ module scrypt.password;
 import scrypt.crypto_scrypt;
 import std.string : indexOf;
 import std.exception : enforce;
+import std.digest.sha : sha1Of;
+import std.digest.digest : toHexString;
+import std.uuid : randomUUID;
 
 enum SCRYPT_N_DEFAULT = 16384;
 enum SCRYPT_R_DEFAULT = 8;
 enum SCRYPT_P_DEFAULT = 1;
 enum SCRYPT_OUTPUTLEN_DEFAULT = 90;
-enum SCRYPT_SALT_SEPERATOR_DEFAULT = '$';
+enum SCRYPT_SALT_SEPERATOR = '$';
 
-
-string generateRandomSalt(size_t length = 32, immutable char unique_char = SCRYPT_SALT_SEPERATOR_DEFAULT) {
-    // TODO: You'll know what to do
-    return "";
+string genRandomSalt() {
+    return randomUUID().toString();
 }
 
 /**
-  * genScryptPassword(string password, string salt, size_t outputlen, ulong N, uint r, uint p, immutable char salt_seperator
+  * genScryptPasswordHash(string password, string salt, size_t outputlen, ulong N, uint r, uint p
   * 
   * Some info regarding the params
   * password: The password you want to hash, for example "my password"
   * salt: The salt you want to use when hashing your password, for example generateRandomSalt();
-  * outputlen: the length of the output string containing your hashed password. Reccomended value is 90.
+  * outputlen: the length of the output string containing your hashed password. Reccomended value is 90. Note, the actual output won't be 90 since it's a sha1 digest
   * N: General work factor, iteration count. Must be power of two. Recommended value for passwords: 2^14 and 2^20 for sensitive stuff
   * r: Blocksize for underlying hash. Reccommended value is 8
   * p: parallelization factor. Reccomended value is 1
-  * salt_seperator: Whatever you passed to generateRandomSalt as the unique_char argument, the default is $
   * If you want to you can use SCRYPT_N_DEFAULT, SCRYPT_R_DEFAULT, SCRYPT_P_DEFAULT, SCRYPT_OUTPUTLEN_DEFAULT as default params
   */
-string genScryptPassword(string password, string salt, size_t outputlen, ulong N, uint r, uint p, immutable char salt_seperator = SCRYPT_SALT_SEPERATOR_DEFAULT) {
+string genScryptPasswordHash(string password, string salt, size_t outputlen, ulong N, uint r, uint p) {
     ubyte[] outpw = new ubyte[outputlen];
     crypto_scrypt(cast(ubyte*)password.ptr, password.length, cast(ubyte*)salt.ptr, salt.length, N, r, p, outpw.ptr, outpw.length);
     
-    return salt ~ salt_seperator ~ cast(string)outpw.idup;
+    return salt ~ SCRYPT_SALT_SEPERATOR ~ toHexString(sha1Of(outpw)).idup;
 }
 
-bool checkScryptPassword(string hash, string password, size_t outputlen = SCRYPT_OUTPUTLEN_DEFAULT, ulong N = SCRYPT_N_DEFAULT, uint r = SCRYPT_R_DEFAULT, uint p = SCRYPT_P_DEFAULT, immutable char salt_seperator = SCRYPT_SALT_SEPERATOR_DEFAULT) {
-    long sep_index = indexOf(hash, salt_seperator);
+bool checkScryptPasswordHash(string hash, string password, size_t outputlen, ulong N, uint r, uint p) {
+    long sep_index = indexOf(hash, SCRYPT_SALT_SEPERATOR);
     enforce(sep_index >= 0, "could not find the seperator index");
-    return genScryptPassword(password, hash[0 .. sep_index], outputlen, N, r, p, salt_seperator) == hash;
+    return genScryptPasswordHash(password, hash[0 .. sep_index], outputlen, N, r, p) == hash;
 }
 
-unittest {
-}
