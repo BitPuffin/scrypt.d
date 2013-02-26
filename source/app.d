ed82544d7f64015a760c79da0bba0470bc4076c4
Isak Andersson <BitPuffin@lavabit.com>
2013-02-26 15:30:51 +0100

    tidy up example

--- source/app.d
+++ source/app.d
@@ -2,12 +2,5 @@ import std.stdio;
 import scrypt.password;
 
 void main() {
-    auto pw = genScryptPassword("my password", "penis");
-    writeln(checkScryptPassword(pw, "my password") ? "it matched!" : "wtf");
-    writeln(genScryptPassword("balls", "salt1")[0 .. 5] != genScryptPassword("balls", "salt2")[0.. 5] ? "They are different hashes!" : "wtf");
-    writeln(genScryptPassword("balls", "salt1"), "\n\n");
-    writeln(genScryptPassword("balls", "salt2"), "\n\n");
-    writeln(genScryptPassword("balls", "salt2"), "same salt gives same shit \n\n");
-
-    writeln("just trying params:\n", genScryptPassword("password", "", 300, 64, 16, 1, '$')); 
+    writeln("example: ", genScryptPassword("password", generateRandomSalt(), SCRYPT_OUTPUTLEN_DEFAULT, SCRYPT_N_DEFAULT, SCRYPT_R_DEFAULT, SCRYPT_P_DEFAULT, SCRYPT_SALT_SEPERATOR_DEFAULT)); 
 }
